21be97cad4a1fa8a21a9cc9a98b4fac6847168ea
Matias Ponce <matias.ponce@mulesoft.com>
2024-05-10 00:34:43 -0300

    Solved

--- lists.py
+++ lists.py
@@ -1,20 +1,37 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) > 5:
+        del list_to_remove_elements[5]
+    if len(list_to_remove_elements) > 4:
+        del list_to_remove_elements[4]
+    if len(list_to_remove_elements) > 0:
+        del list_to_remove_elements[0]
+    return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.append("Yellow")
+    # list_to_add_elements.insert(len(list_to_add_elements), 'Yellow')
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return len(list_to_check) == 0
 
 
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_lists(list1, list2):
+    if len(list1) < 3 or len(list2) < 3:
+        return False
+    elif list1[2] == list2[2]:
+        return True
+    elif list1[2] != list2[2]:
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    element1 = list_of_lists_to_modify[0][0:2]
+    element2 = list_of_lists_to_modify[1][1:4]
+    element3 = list_of_lists_to_modify[2][-2:]
+    return [element1, element2, element3]
